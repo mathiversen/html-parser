68ac563102d8adb2064b384e8da372bf004f5554
Mathias Iversen <work@mathiasiversen.com>
2020-05-14 20:44:20 +0200

    added support for nested elements
    this commit makes it possible to parse elements that are nested
    <div>
        <div/>
    </div>

--- src/parser/tests.rs
+++ src/parser/tests.rs
@@ -2,11 +2,11 @@ use super::*;
 use anyhow::Result;
 use indoc::indoc;
 
+#[ignore]
 #[test]
 fn it_works() {
     assert!(true)
 }
-
 #[ignore]
 #[test]
 fn it_can_parse_a_document() {
@@ -22,13 +22,40 @@ fn it_can_parse_a_document() {
     let result = HtmlParser::parse(markup);
     assert!(result.is_ok());
 }
+#[ignore]
+#[test]
+fn it_can_parse_document_without_doctype() {
+    let markup = indoc!(
+        r#"
+        <!-- comment -->
+        <!-- comment -->
+        <html></html>
+        <!-- comment -->
+    "#
+    );
+    let result = HtmlParser::parse(markup);
+    assert!(result.is_ok());
+}
 
+#[ignore]
 #[test]
 fn it_can_parse_a_document_fragment() {
     let markup = indoc!(
         r#"
         <div id="test" class="kalle 123"></div>
         <img width="123" height="321" />
+    "#
+    );
+    let result = HtmlParser::parse(markup);
+    dbg!(&result);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn it_can_parse_nested_elements() {
+    let markup = indoc!(
+        r#"
+        <div class="abc" style="abc"><div class="bcd" /></div>
     "#
     );
     let result = HtmlParser::parse(markup);
