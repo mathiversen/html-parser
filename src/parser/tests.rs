d2ed8b8c5ad009878baaf95b795d5aba28865df9
Mathias Iversen <work@mathiasiversen.com>
2020-05-14 17:16:48 +0200

    able to parse open and closed elements
    this commit adds grammar that enables parsing of open and closed
    elements, as such:
    - <div one="one" two="two">content</div> (open)
    - <img alt="hello" /> (closed)
    
    ive also saved a bunch of html tags in a temporary html file, i first
    though that i could implment it into the parser, if a html-tag is valid
    or not, but i think it's nicer to do it when you've got the tree?

--- src/parser/tests.rs
+++ src/parser/tests.rs
@@ -1,15 +1,37 @@
 use super::*;
 use anyhow::Result;
+use indoc::indoc;
 
 #[test]
 fn it_works() {
     assert!(true)
 }
 
+#[ignore]
 #[test]
-fn it_can_parse() -> Result<()> {
-    let result = HtmlParser::parse("hell world")?;
-    dbg!(result);
-    assert!(false);
-    Ok(())
+fn it_can_parse_a_document() {
+    let markup = indoc!(
+        r#"
+        <!-- comment -->
+        <!doctype html>
+        <!-- comment -->
+        <html></html>
+        <!-- comment -->
+    "#
+    );
+    let result = HtmlParser::parse(markup);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn it_can_parse_a_document_fragment() {
+    let markup = indoc!(
+        r#"
+        <div id="test" class="kalle 123"></div>
+        <img width="123" height="321" />
+    "#
+    );
+    let result = HtmlParser::parse(markup);
+    dbg!(&result);
+    assert!(result.is_ok());
 }
