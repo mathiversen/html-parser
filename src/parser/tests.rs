088673ed2f46661a492bf30a1c1e31091b5648c5
Mathias Iversen <work@mathiasiversen.com>
2020-05-15 13:28:02 +0200

    major parser test rewrite
    this commit re-implements *all* of the tests for the parser. with most
    of the core logic in place in the parser i thought it was a good
    approach to use TDD and walk to the program and see that all of the
    tests would pass. i sort of came to this conclusion after just a small
    idea that i should pass another arg to the parser that would let me
    debug only one test at the time, meanwhile still having all of the tests
    running all of the time. this is a really nice setup as im catching all
    of the errors as i do changes to the pest parser!

--- src/parser/tests.rs
+++ src/parser/tests.rs
@@ -2,97 +2,250 @@ use super::*;
 use anyhow::Result;
 use indoc::indoc;
 
-#[ignore]
 #[test]
-fn it_works() {
-    assert!(true)
+fn it_can_parse_empty_document() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_one_element() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<html></html>", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_one_element_upper_case() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<HTML></HTML>", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_one_element_mixed_case() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<Html></Html>", false)?);
+    Ok(())
+}
+#[test]
+fn it_errors_when_case_dont_match() -> Result<()> {
+    assert!(HtmlParser::parse("<html></Html>", false).is_err());
+    Ok(())
+}
+#[test]
+fn it_errors_on_element_without_matching_name() -> Result<()> {
+    assert!(HtmlParser::parse("<html></div>", false).is_err());
+    Ok(())
+}
+#[test]
+fn it_can_parse_multiple_elements() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<div></div><div></div>", false)?);
+    Ok(())
+}
+#[test]
+fn it_errors_when_multiple_elements_dont_match() -> Result<()> {
+    assert!(HtmlParser::parse("<div></span><div></div>", false).is_err());
+    Ok(())
+}
+#[test]
+fn it_can_parse_one_comment() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<!-- hello !\"#/()= -->", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_multiple_comments() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<!--x--><!--y--><!--z-->", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_one_text() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("hello world", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_multiple_rows_of_text() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("hello\nworld\n!", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_element_comment_text() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<div></div><!--x-->hello", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_nested_elements() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<div><div></div></div>", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_open_element_attribute_double_quote() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<div id=\"one\"></div>", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_closed_element_attribute_double_quote() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<img alt=\"cat\" />", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_open_element_attribute_single_quote() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<div id='one'></div>", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_closed_element_attribute_single_quote() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<img alt='cat'/>", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_multiple_attributes_single_quote() -> Result<()> {
+    assert_eq!(
+        (),
+        HtmlParser::parse("<img alt='cat bog sheep-123'/>", false)?
+    );
+    Ok(())
+}
+#[test]
+fn it_can_parse_multiple_attributes_double_quote() -> Result<()> {
+    assert_eq!(
+        (),
+        HtmlParser::parse("<img alt=\"cat bog sheep-123\"/>", false)?
+    );
+    Ok(())
+}
+#[test]
+fn it_can_parse_empty_attributes() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<img hidden/>", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_nested_elements_comments_text() -> Result<()> {
+    assert_eq!(
+        (),
+        HtmlParser::parse("<p id='body'><i>hello</i><!--x-->world</p>", false)?
+    );
+    Ok(())
+}
+#[test]
+fn it_can_parse_nested_and_indented() -> Result<()> {
+    let markup = indoc!(
+        r#"
+            <p id='body'>
+                <i>hello</i>
+                <!--x-->
+                world
+            </p>
+        "#
+    );
+    assert_eq!((), HtmlParser::parse(markup, false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_deeply_nested() -> Result<()> {
+    let markup = indoc!(
+        r#"
+            <div class='1'>
+                <div class='1'>
+                    <div class='1'>
+                        <div class='1'>
+                            <div class='1'>
+                                <div class='1'>
+                                    <div class='1'>
+                                        <div class='1'>
+                                            <!--this is deep-->
+                                            hello world
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        "#
+    );
+    assert_eq!((), HtmlParser::parse(markup, false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_script_with_content() -> Result<()> {
+    let markup = indoc!(
+        r#"
+            <script>
+                const person_creator = ({ name, symtoms }) => {
+                    let person = {}
+                    person.name = name
+                    person.symtoms = {}
+                    for (symtom of symtoms) {
+                        person.symtoms[symtom] = true
+                    }
+                    return person
+                }
+                
+                const main = () => {
+                    let name = 'mathias'
+                    let symtoms = ['Dunning-Kruger', 'ACDC', 'Slacker']
+                
+                    setTimeout(() => {
+                        let person = person_creator({ name, symtoms })
+                        if (person.symtoms.hasOwnProperty('Dunning-Kruger')) {
+                            console.log('yeah buddy, that\'s right')
+                        }
+                    }, 1337)
+                }
+                
+                main()
+            </script>
+        "#
+    );
+    assert_eq!((), HtmlParser::parse(markup, false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_style_with_content() -> Result<()> {
+    let markup = indoc!(
+        r#"
+            <style>
+                :root {
+                    --background-color: black;
+                    --text-color: white;
+                }
+                body {
+                    background: var(--background-color);
+                    color: var(--text-color);
+                }
+            </style>
+        "#
+    );
+    assert_eq!((), HtmlParser::parse(markup, false)?);
+    Ok(())
 }
 #[ignore]
 #[test]
-fn it_can_parse_a_document() {
+fn it_can_parse_simple_html_page() -> Result<()> {
     let markup = indoc!(
         r#"
-        <!-- comment -->
-        <!doctype html>
-        <!-- comment -->
-        <html></html>
-        <!-- comment -->
-    "#
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+                <style>
+                    body {
+                        background: black;
+                    }
+            
+                    h1 {
+                        color: white;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>Hello world</h1>
+                <script>
+                    const title = document.querySelector("h1")
+                    title.innerText = "Hello from script"
+                </script>
+            </body>
+            </html>        
+        "#
     );
-    let result = HtmlParser::parse(markup);
-    assert!(result.is_ok());
-}
-#[ignore]
-#[test]
-fn it_can_parse_document_without_doctype() {
-    let markup = indoc!(
-        r#"
-        <!-- comment -->
-        <!-- comment -->
-        <html></html>
-        <!-- comment -->
-    "#
-    );
-    let result = HtmlParser::parse(markup);
-    assert!(result.is_ok());
-}
-
-#[ignore]
-#[test]
-fn it_can_parse_a_document_fragment() {
-    let markup = indoc!(
-        r#"
-        <div id="test" class="kalle 123"></div>
-        <img width="123" height="321" />
-    "#
-    );
-    let result = HtmlParser::parse(markup);
-    dbg!(&result);
-    assert!(result.is_ok());
-}
-
-#[ignore]
-#[test]
-fn it_can_parse_nested_elements() {
-    let markup = indoc!(
-        r#"
-        <div class="abc" style="abc"><div class="bcd" /></div>
-    "#
-    );
-    let result = HtmlParser::parse(markup);
-    dbg!(&result);
-    assert!(result.is_ok());
-}
-
-#[ignore]
-#[test]
-fn it_can_parse_deep_nested_elements() {
-    let markup = indoc!(
-        r#"
-        <div><p><span></span></p></div>
-    "#
-    );
-    let result = HtmlParser::parse(markup);
-    dbg!(&result);
-    assert!(result.is_ok());
-}
-
-#[test]
-fn it_can_parse_all_node_types_when_nested() {
-    let markup = indoc!(
-        r#"
-        <div>
-            <!-- Comment here yao -->
-            <div/>
-            <p><!-- hello <!-@£$€¥> -->text</p>
-            text2
-            text2
-            text2
-            <div><div>123</div>321</div>
-        </div>
-    "#
-    );
-    let result = HtmlParser::parse(markup);
-    println!("{:?}", &result);
-    assert!(result.is_ok());
+    assert_eq!((), HtmlParser::parse(markup, false)?);
+    Ok(())
 }
