5de930299d7b1ec943f6d17ab8dcd323fbab5ab6
Mathias Iversen <work@mathiasiversen.com>
2020-05-18 12:55:47 +0200

    added attr test for non-quoted attrs

--- src/parser/tests/attributes.rs
+++ src/parser/tests/attributes.rs
@@ -2,18 +2,28 @@ use crate::parser::HtmlParser;
 use anyhow::Result;
 
 #[test]
-fn it_can_parse_open_element_attribute_double_quote() -> Result<()> {
+fn it_can_parse_double_quote() -> Result<()> {
     assert_eq!((), HtmlParser::parse("<div id=\"one\"></div>", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_single_quote() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<div id='one'></div>", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_no_quote_no_space() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<div id=one></div>", false)?);
+    Ok(())
+}
+#[test]
+fn it_can_parse_no_quote_space() -> Result<()> {
+    assert_eq!((), HtmlParser::parse("<div id=one ></div>", false)?);
     Ok(())
 }
 #[test]
 fn it_can_parse_closed_element_attribute_double_quote() -> Result<()> {
     assert_eq!((), HtmlParser::parse("<img alt=\"cat\" />", false)?);
-    Ok(())
-}
-#[test]
-fn it_can_parse_open_element_attribute_single_quote() -> Result<()> {
-    assert_eq!((), HtmlParser::parse("<div id='one'></div>", false)?);
     Ok(())
 }
 #[test]
